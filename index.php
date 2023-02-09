14d3b11211b8cc04133658cc6fcbd91e54117192
Henintsoa Micael Andriamahadimby <handri5@a-999903.ups.u-psud.fr>
2023-02-09 11:34:09 +0100

    add bootstrap and jQ

--- index.php
+++ index.php
@@ -1,2 +1,25 @@
-<?php
-echo "hello world";
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta name="description" content="Webpage description goes here" />
+        <meta charset="utf-8">
+        <title>Change_me</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="author" content="">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" 
+            integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+        <link rel="stylesheet" href="style.css">
+    </head>
+
+    <body>
+        <header></header>
+        <main>Liste des tâches</main>
+        <footer></footer>
+    <script>
+        <script src="https://code.jquery.com/jquery-3.6.3.slim.min.js"
+            integrity="sha256-ZwqZIVdD3iXNyGHbSYdsmWP//UBokj2FHAxKuSBKDSo=" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" 
+            integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>    
+    </script>
+    </body>
+</html>
